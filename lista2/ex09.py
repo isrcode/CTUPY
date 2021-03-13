e61dd3cfdba1d5e87bb0a1eb998593649cba5b6e
isrcode <rm89347@fiap.com.br>
2021-03-13 20:34:38 -0300

    ex09

--- lista2/ex09.py
+++ lista2/ex09.py
@@ -4,3 +4,10 @@ d = float(input("Qual o percentual de desconto a ser aplicado? ").replace(',', '
 pn = p - (p/100*d)
 
 print(str(f"O produto custa R${p:.2f} com desconto de {d}% fica por R${pn:.2f}").replace('.', ','))
+
+""" 
+Se ao invés de um desconto fosse um aumento,
+o que mudaria no codigo seria pn = p + (p/100*d)
+e a mensagem no print descrevendo um almento e não
+um desconto.
+"""
