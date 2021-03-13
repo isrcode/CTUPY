ce1fc81ed01a420f77b6cdd49c2115ce36b824ea
isrcode <rm89347@fiap.com.br>
2021-03-13 20:02:11 -0300

    ex08

---
+++ lista2/ex08.py
@@ -0,0 +1,7 @@
+camisa = int(input("Qual a quantidade de camisas? "))
+calças = int(input("Qual a quantidade de calças? "))
+sapatos = int(input("Qual a quantidade de sapatos? "))
+
+conbinações = camisa * calças * sapatos
+
+print(f"Dadas as quantidades inseridas são possíveis {conbinações} conbinações diferentes.")
