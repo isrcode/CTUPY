b51b6da0fcae6115e0ed9f5935da09e3bb05d3ac
isrcode <rm89347@fiap.com.br>
2021-04-25 11:24:32 -0300

    Resoluçao exercicio 10 da lista 4

---
+++ Lista04_Exercicio/ex_10.py
@@ -0,0 +1,14 @@
+n = int(input("Digite um numero: "))
+n_mult = 1
+mult = 1
+
+while mult <= n:
+    n_mult = n_mult * mult
+    mult += 1
+print(n_mult)
+
+
+"""
+10. Escreva um algoritmo que recebe um inteiro positivo n e calcula n! = 1 · 2 · 3 . . . · (n − 1) · n .
+Por exemplo, se n = 6 , então 6! = 1 · 2 · 3 · 4 · 5 · 6 = 720 .
+"""
